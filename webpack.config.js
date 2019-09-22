4cfd8a56feeae7be22c05b4e562010da7e8e32e4
firekilin <wl00161839@gmail.com>
2019-09-22 16:42:57 +0800

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+const path = require("path");
+const rootPath = path.join(__dirname,'./');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+//抽出css檔案
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+//自動產生html 靜態檔案對應
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+module.exports = {
+    mode: process.env.NODE_ENV || 'development',
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        filename: '[name].[chunkhash].js',          //filename: '[name].js',
+        path: path.resolve(__dirname, 'build')
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/, // /\.(sass|scss|css)$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: false,
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ],
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/, // /\.(sass|scss|css)$/,
+                use: [
+
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            options:{limit:1000}
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            byPassOnDebug:true
+                        }
+                    }
+
+                ],
+            }
+        ]   //rules end
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+        new HtmlWebpackPlugin({
+            inject: false,
+            hash: false,
+            template: './src/index.html',
+            filename: 'index.html'
+        })
+    ],
+    devServer: {
+        open: true,
+        contentBase: path.join(__dirname, '.'),
+        compress: true,
+        port: 9007
+    }
+};
